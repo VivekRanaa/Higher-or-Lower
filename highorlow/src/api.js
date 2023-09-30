da935c2b042458edab2c9a27b1dd79b747569251
Vivekranaa <vrana3325@gmail.com>
2023-09-30 19:44:47 +0530

    updated

--- highorlow/src/api.js
+++ highorlow/src/api.js
@@ -1,14 +1,12 @@
+import axios from "axios";
 
-import axios from 'axios';
-
-const API_KEY = 'rr2z0CSANtKoa1DrHFBVyb69TwJWHR_4fDufb31TfFI';
-
+const API_KEY = "rr2z0CSANtKoa1DrHFBVyb69TwJWHR_4fDufb31TfFI";
 
 export async function fetchImageByKeyword(keyword) {
   try {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       headers: {
-        'Cache-Control':'no-cache'
+        "Cache-Control": "no-cache",
       },
       params: {
         query: keyword,
@@ -16,17 +14,16 @@ export async function fetchImageByKeyword(keyword) {
       },
     });
 
-  
     if (response.data.results && response.data.results.length > 0) {
       const imageUrl = response.data.results[0].urls.regular;
-      console.log('Fetched image URL:', imageUrl); 
+      console.log("Fetched image URL:", imageUrl);
       return imageUrl;
     } else {
-      console.log('No results found for keyword:', keyword);
+      console.log("No results found for keyword:", keyword);
       return null;
     }
   } catch (error) {
-    console.error('Error fetching image:', error);
+    console.error("Error fetching image:", error);
     return null;
   }
 }
